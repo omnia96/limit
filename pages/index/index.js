a0d095827cc76ff2e2f8fdfaf355443c0850f7bc
unknown <unknown@example.com>
2019-05-10 09:24:49 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,127 @@
+//index.js
+//获取应用实例
+const app = getApp()
+var WXP = require('../../lib/wxp.min.js').default
+var QQMapWX = require('../../lib/qqmap-wx-jssdk.js')
+Page({
+  data: {
+    lunarInfo: null,
+    current: 'tab1',
+    current_scroll: 'tab1',
+    rules: null,
+    showRules: null
+  },
+  onLoad: function () {
+    this.initCache()
+    // if(this.lunarInfoIf(date)){
+    //   console.log("start set data")
+    //   console.log(this.lunarInfoIf(date))
+    // }else{
+    //   console.log("start requst data")
+    //   this.getLunarInfo(date)
+    // }
+  },
+  onShow() {
+    let that = this
+    that.getUserLocation()
+    that.getRules()
+  },
+  handleChange({detail}) {
+    console.log(detail)
+    let rules = this.data.rules
+    if (detail.key == "tab1") {
+      this.setData({
+        showRules: rules.local
+      })
+    } else if (detail.key == "tab2") {
+      this.setData({
+        showRules: rules.outland
+      })
+    }
+    this.setData({
+      current: detail.key
+    });
+  },
+
+  handleChangeScroll({
+    detail
+  }) {
+    this.setData({
+      current_scroll: detail.key
+    });
+  },
+  //获取位置
+  getUserLocation: function () {
+    let that = this
+    WXP.getLocation({
+      type: 'gcj02'
+    }).then(result => {
+      console.log('success信息:', result)
+      let location = {
+        latitude: result.latitude,
+        longitude: result.longitude
+      }
+      that.getUserCityName(location)
+    }).catch(error => {
+
+    })
+  },
+  getUserCityName(location) {
+    let that = this
+    let qqmapsdk = new QQMapWX({
+      key: 'QG2BZ-4OS3U-QNUVG-4RYJG-C54ZZ-3ZFCW'
+    })
+    qqmapsdk.reverseGeocoder({
+      location: location,
+      success: function (res) {
+        let location = res.result.address_component
+        console.log(location)
+      }
+    })
+  },
+  getRules() {
+    let that = this
+    WXP.request({
+      url: "http://localhost/wechatsever/xx/returndata.php",
+      header: "Content-Type:application/json"
+    }).then(result => {
+      console.log(result)
+      that.setData({
+        rules: result.data.data,
+        showRules: result.data.data.local
+      })
+    }).catch(error => {
+
+    })
+  },
+
+  initCache() {
+    let userLunarInfo = null
+    userLunarInfo = app.getCache("userLunarInfo")
+    if (userLunarInfo === false) {
+      app.setCache("userLunarInfo", {
+        "msg": false,
+        "data": {
+          "defaut": "默认"
+        }
+      })
+    }
+  },
+  lunarInfoIf(date) {
+    let userLunarInfo = app.getCache("userLunarInfo")
+    console.log(userLunarInfo)
+    if (userLunarInfo["msg"] == true) {
+      //存在缓存数据
+      var data = userLunarInfo["data"]
+      if (data[date]) {
+        return data[date]
+      } else {
+        return false
+      }
+    } else {
+      console.log("没有数据")
+      //请求数据
+      return false
+    }
+  }
+})
---
+++ pages/limitDetails/limitDetails.js
@@ -0,0 +1,123 @@
+// pages/limitDetails/limitDetails.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    longitude:108.93984,
+    latitude:34.34127,
+    polygon:[{
+      points: [
+        {
+            "longitude": 108.78525582520594, 
+            "latitude": 34.2998849667663
+        }, 
+        {
+            "longitude": 108.84003991446573, 
+            "latitude": 34.34359376436034
+        }, 
+        {
+            "longitude": 108.92153339407848, 
+            "latitude": 34.36170229130376
+        }, 
+        {
+            "longitude": 108.9561760740064, 
+            "latitude": 34.365533906089205
+        }, 
+        {
+            "longitude": 109.06999096435074, 
+            "latitude": 34.35031230219455
+        }, 
+        {
+            "longitude": 109.09644085928466, 
+            "latitude": 34.313188747063755
+        }, 
+        {
+            "longitude": 109.08957306318707, 
+            "latitude": 34.27637772094279
+        }, 
+        {
+            "longitude": 109.00603191538336, 
+            "latitude": 34.19669132882823
+        }, 
+        {
+            "longitude": 108.82566519833654, 
+            "latitude": 34.19095588734232
+        }, 
+        {
+            "longitude": 108.82566519833654, 
+            "latitude": 34.19095588734232
+        }
+    ],
+      // color: '#FF0000DD',
+      // width: 2,
+      // dottedLine: true
+      strokeWidth:2,
+      strokeColor:"#000000",
+      fillColor:"#000fff50",
+      zIndex:1
+    }]
+  },
+  markertap(e) {
+    console.log(e)
+  },
+  controltap(e) {
+    console.log(e.controlId)
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
